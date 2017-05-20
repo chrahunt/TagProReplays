fbe6789039d7f7e4d6bfd4685060c2e6f06e1a93
Chris Hunt <chrahunt@gmail.com>
2017-05-19 20:33:52 -0400

    Release 1.3.16.

---
+++ tools/release.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+usage() {
+    echo Usage:
+    echo "release.sh <version>"
+}
+
+if [ -z "$1" ]; then
+    usage
+    exit 1
+fi
+version="$1"
+git checkout master                         \
+&& sed --in-place \
+     "s/\"version\": \".*\"/\"version\": \"$version\"/g" \
+     package.json                           \
+&& gulp build-release                       \
+&& cd dist && zip -o dist.zip -r * && cd .. \
+&& git add .                                \
+&& git commit -m "Release $version."        \
+&& git tag -a -m "v$version" v$version      \
+&& git push origin master                   \
+&& git push origin v$version
