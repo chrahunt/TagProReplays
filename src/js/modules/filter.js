b62ec0de8a19b8ba324cdb407894a95b289f23ac
ballparts <ballparts@ballpartss-Mac-mini.local>
2018-04-08 13:45:56 -0400

    Add space after 'if'.

--- src/js/modules/filter.js
+++ src/js/modules/filter.js
@@ -16,13 +16,13 @@ function filter(metadata, query) {
 
     try {
       let queryObject = parser.parse(query, {keywords: keywords});
-      if(queryObject === "") resolve(metadata);
+      if (queryObject === "") resolve(metadata);
     
-      if(typeof(queryObject) === "string") queryObject = {text: queryObject};
+      if (typeof(queryObject) === "string") queryObject = {text: queryObject};
 
 
       let results = metadata.filter(filterReplay.bind(this, queryObject));
-      if(!results) reject();
+      if (!results) reject();
       resolve(results);
     } catch(err) {
       reject(err);
@@ -39,8 +39,8 @@ function filter(metadata, query) {
 */
 function flatten(replayMetadata) {
   replayMetadata.player = [];
-  if(replayMetadata.red_team) replayMetadata.player = replayMetadata.player.concat(replayMetadata.red_team);
-  if(replayMetadata.blue_team) replayMetadata.player = replayMetadata.player.concat(replayMetadata.blue_team);
+  if (replayMetadata.red_team) replayMetadata.player = replayMetadata.player.concat(replayMetadata.red_team);
+  if (replayMetadata.blue_team) replayMetadata.player = replayMetadata.player.concat(replayMetadata.blue_team);
   replayMetadata.player = replayMetadata.player.join(' ');
   replayMetadata.text = `${replayMetadata.map} ${replayMetadata.name} ${replayMetadata.player}`; // makes searching all text simpler
   return replayMetadata;
@@ -57,59 +57,59 @@ function filterReplay(queryObject, replayMetadata) {
   let matched = true;
   
   // handle excludes
-  if(queryObject.exclude) {  
+  if (queryObject.exclude) {  
     let excluded = Object.keys(queryObject.exclude).some(item => {
-      if(typeof(queryObject.exclude[item]) === "string") 
+      if (typeof(queryObject.exclude[item]) === "string") 
         queryObject.exclude[item] = [queryObject.exclude[item]];
       return queryObject.exclude[item].some(excludedQuery => {
         return flatMetadata[item].toLowerCase().includes(excludedQuery.toLowerCase());
       });
     });
-    if(excluded) return false;
+    if (excluded) return false;
   }
 
   // handle maps - handled as OR, so matching any is acceptable
-  if(queryObject.map) {
-    if(typeof(queryObject.map) === "string") queryObject.map = [queryObject.map];
+  if (queryObject.map) {
+    if (typeof(queryObject.map) === "string") queryObject.map = [queryObject.map];
     let matchedMap = queryObject.map.some(mapQuery => {
       return flatMetadata.map.toLowerCase().includes(mapQuery.toLowerCase());
     });
-    if(!matchedMap) matched = false;
+    if (!matchedMap) matched = false;
   }
 
   // handle players - handled as AND, so must match all
-  if(queryObject.player) {
-    if(typeof(queryObject.player) === "string") queryObject.player = [queryObject.player];
+  if (queryObject.player) {
+    if (typeof(queryObject.player) === "string") queryObject.player = [queryObject.player];
     let matchedPlayer = queryObject.player.every(playerQuery => {
       return flatMetadata.player.toLowerCase().includes(playerQuery.toLowerCase());
     });
-    if(!matchedPlayer) matched = false;
+    if (!matchedPlayer) matched = false;
   }
 
   // handle name - handled as OR, so matching any is acceptable
-  if(queryObject.name) {
-    if(typeof(queryObject.name) === "string") queryObject.name = [queryObject.name];
+  if (queryObject.name) {
+    if (typeof(queryObject.name) === "string") queryObject.name = [queryObject.name];
     let matchedName = queryObject.name.some(nameQuery => {
       return flatMetadata.name.toLowerCase().includes(nameQuery.toLowerCase());
     });
-    if(!matchedName) matched = false;
+    if (!matchedName) matched = false;
   }
 
   // handle free text - terms will be split by white space, unless within double quotes.
   // this will handle exclusions in free text as well
   // They are then treated as AND, so all must match
-  if(queryObject.text) {
+  if (queryObject.text) {
     let textTerms = formatText(queryObject.text);
 
     let excludedText = textTerms.exclude.some(term => {
       return flatMetadata.text.toLowerCase().includes(term.toLowerCase());
     });
-    if(excludedText) return false;
+    if (excludedText) return false;
 
     let matchedText = textTerms.include.every(term => {
       return flatMetadata.text.toLowerCase().includes(term.toLowerCase());
     });
-    if(!matchedText) matched = false;
+    if (!matchedText) matched = false;
   }
 
   return matched;
