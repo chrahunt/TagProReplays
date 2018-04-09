ab776fc0461f002a67d2b9e25d92c7f5f2e23a51
ballparts <ballparts@ballpartss-Mac-mini.local>
2018-04-08 22:27:34 -0400

    attempt to parameterize filter testing.

--- test/filter.spec.js
+++ test/filter.spec.js
@@ -6,122 +6,28 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 // load example metadata
-function load_metadata(metadata_file) {
-  let path = `fixtures/filter/${metadata_file}.json`;
+function load_data(data_file) {
+  let path = `fixtures/filter/${data_file}.json`;
   return fetch(path).then((res) => res.json());
 }
 
-load_metadata('example_metadata').then((metadata) => {
-  
+const metadataPromise = load_data('example_metadata');
+const testDataPromise = load_data('test_data_file');
+
+Promise.all([metadataPromise, testDataPromise], (dataArray) => {
+  const metadata = dataArray[0];
+  const testData = dataArray[1];
 
   describe('filter test', () => {
 
-    it('should return all results with empty query', () => {
-      let results = filter(metadata, '');
-      expect(results).to.eventually.have.lengthOf(22);
+    testData.forEach(test => {
+      it(`should ${test.name}`, () => {
+        filter(metadata, test.query)
+          .then(results => {
+            let resultsIDs = results.map(result => result.id);
+            expect(resultsIDs).to.deep.equal(test.expected);
+          });
+      });
     });
-
-    // map tests
-    it('should filter by single map', () => {    
-      let results = filter(metadata, 'map:Gumbo');
-      expect(results).to.eventually.have.lengthOf(4);
-    });
-
-    it('should partial match map insensitive to case', () => {
-      let results = filter(metadata, 'map:gum');
-      expect(results).to.eventually.have.lengthOf(4);
-    });
-
-    it('should filter by multiple maps', () => {
-      let results = filter(metadata, 'map:Gumbo map:Pilot');
-      expect(results).to.eventually.have.lengthOf(5);  
-    })
-
-    // player tests
-    it('should filter by single player', () => {
-      let results = filter(metadata, 'player:ballparts');
-      expect(results).to.eventually.have.lengthOf(9);
-    });
-
-    it('should partial match player insensitive to case', () => {
-      let results = filter(metadata, 'player:BALLP');
-      expect(results).to.eventually.have.lengthOf(9);
-    });
-
-    it('should filter by multiple players', () => {
-      let results = filter(metadata, 'player:ballparts player:Some');
-      expect(results).to.eventually.have.lengthOf(7);
-    });
-
-    // name tests
-    it('should filter by single name term', () => {
-      let results = filter(metadata, 'name:replay');
-      expect(results).to.eventually.have.lengthOf(15);
-    });
-
-    it('should partial match name insensitive to case', () => {
-      let results = filter(metadata, 'name:NUMB');
-      expect(results).to.eventually.have.lengthOf(3);
-    });
-
-    it('should filter by multiple name terms', () => {
-      let results = filter(metadata, 'name:number name:button name:pup');
-      expect(results).to.eventually.have.lengthOf(5);
-    });
-
-    // free text tests
-    it('should filter by single non-keyword terms', () => {
-      let results = filter(metadata, 'IRON');
-      expect(results).to.eventually.have.lengthOf(3);
-    });
-
-    it('should partial match text insensitive to case', () => {
-      let results = filter(metadata, 'iro');
-      expect(results).to.eventually.have.lengthOf(3);
-    });
-
-    it('should filter by multiple non-keyword terms', () => {
-      let results = filter(metadata, 'IRON FILTER');
-      expect(results).to.eventually.have.lengthOf(2);
-    });
-
-    // exclusion tests
-    it('should exclude single keyword terms', () => {
-      let results1 = filter(metadata, '-map:Gumbo');
-      expect(results1).to.eventually.have.lengthOf(18);
-
-      let results2 = filter(metadata, '-player:ballparts');
-      expect(results2).to.eventually.have.lengthOf(13);
-
-      let results3 = filter(metadata, '-name:replay');
-      expect(results3).to.eventually.have.lengthOf(7);
-    });
-
-    it('should exclude multiple keyword terms', () => {
-      let results = filter(metadata, '-map:Platypus -player:ballparts');
-      expect(results).to.eventually.have.lengthOf(11);
-    });
-
-    it('should exclude single non-keyword terms', () => {
-      let results = filter(metadata, '-IRON');
-      expect(results).to.eventually.have.lengthOf(19);
-    });
-
-    it('should exclude multiple non-keyword terms', () => {
-      let results = filter(metadata, '-IRON -ballparts');
-      expect(results).to.eventually.have.lengthOf(11);
-    });
-
-    // combinations/other
-    it('should deal with quotes properly', () => {
-      let results = filter(metadata, '"Some Ball 4"');
-      expect(results).to.eventually.have.lengthOf(3);
-    });
-
-    it('should combine include and exclude terms', () => {
-      let results = filter(metadata, 'player:ballparts -map:Smirk');
-      expect(results).to.eventually.have.lengthOf(6);
-    });
-
   });
 });
---
+++ tools/create_filter_test_data.js
@@ -0,0 +1,30 @@
+
+// This script generates test data for the filter module's tests.
+// To run, call:
+// node create_filter_test_data.js
+// 
+// results will be printed to stdout
+
+const fs = require('fs');
+const filter = require('../src/js/modules/filter');
+
+const inputDataFile = '../test/fixtures/filter/test_data_generation_file.json';
+const input = JSON.parse(fs.readFileSync(inputDataFile, 'utf8'));
+
+const metadataFile = '../test/fixtures/filter/example_metadata.json';
+const metadata = JSON.parse(fs.readFileSync(metadataFile, 'utf8'));
+
+const results = [];
+
+async function getResults(metadata, item) {
+  let results = await filter(metadata, item.query);
+  item.expected = results.map(result => result.id).sort();
+  return item;
+}
+
+input.forEach((item, i) => {
+  let thisOutput = Object.assign(item, {});
+  results.push(getResults(metadata, item));
+});
+
+Promise.all(results).then(output => console.log(JSON.stringify(output)));
