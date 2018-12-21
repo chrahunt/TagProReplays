1ac5edd789af6194ce9e202b775769c224c6daa5
ylambda <ylambda@koalabeast.com>
2018-12-21 13:36:46 -0500

    Use URL to parse document.URL
    Used to determine if in a game

--- src/js/content.js
+++ src/js/content.js
@@ -24,6 +24,7 @@ const Table = require('modules/table');
 const Upload = require('modules/upload');
 const Search = require('modules/search');
 
+const url = new URL(document.URL);
 // Get URL for setting cookies, assumes a domain of *.hostname.tld:*/etc
 var cookieDomain = document.URL.match(/https?:\/\/[^\/]+?(\.[^\/.]+?\.[^\/.]+?)(?::\d+)?\//)[1];
 
@@ -792,7 +793,7 @@ if ($('#userscript-home').length !== 0) {
   // Include custom bootstrap.css scoped to #tpr-container
   injectStyleSheet("css/bootstrap.css");
   injectStyleSheet("css/menu.css");
-} else if (document.URL.search(/\.\w+:/) >= 0
-        || document.URL.endsWith('/game')) {
+} else if (url.port !== ''
+        || url.pathname === '/game') {
   injectScript('js/recording.js');
 }
